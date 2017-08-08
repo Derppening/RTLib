9dddd64db4b29a1776784346846e57bf0b71caa2
waicool20 <waicool20@gmail.com>
2017-08-08 10:45:02 +0800

    Formatting the cmake files

--- cmake/libopencm3.cmake
+++ cmake/libopencm3.cmake
@@ -1,9 +1,11 @@
+# Also painfully written by waicool20 :)
+
 # Utility
 
 function(JOIN VALUES GLUE OUTPUT)
-    string (REGEX REPLACE "([^\\]|^);" "\\1${GLUE}" _TMP_STR "${VALUES}")
-    string (REGEX REPLACE "[\\](.)" "\\1" _TMP_STR "${_TMP_STR}") #fixes escaping
-    set (${OUTPUT} "${_TMP_STR}" PARENT_SCOPE)
+    string(REGEX REPLACE "([^\\]|^);" "\\1${GLUE}" _TMP_STR "${VALUES}")
+    string(REGEX REPLACE "[\\](.)" "\\1" _TMP_STR "${_TMP_STR}") #fixes escaping
+    set(${OUTPUT} "${_TMP_STR}" PARENT_SCOPE)
 endfunction()
 
 # -----------
