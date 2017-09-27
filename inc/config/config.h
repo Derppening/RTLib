221eb8803529ed7ef387bfc093323f5355026404
Derppening <david.18.19.21@gmail.com>
2017-09-27 16:46:15 +0800

    Config: Add macros for stricter compile-time checking
    - EMPTY_MACRO: Check if a macro is empty
    - Include appropriate standard libraries for utility typedefs

--- inc/config/config.h
+++ inc/config/config.h
@@ -1,6 +1,9 @@
 #ifndef RTLIB_CONFIG_CONFIG_H_
 #define RTLIB_CONFIG_CONFIG_H_
 
+#define EMPTY_MACRO_HELPER(VAL) VAL ## 1
+#define EMPTY_MACRO(VAL) EMPTY_MACRO_HELPER(VAL)
+
 #if defined(STM32F103_DEV)
 #include "stm32f103_dev.h"
 #else
