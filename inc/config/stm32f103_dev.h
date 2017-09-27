c1eacdf479c7fd8464484a1685c76e028463f4a2
David Mak <david.18.19.21@gmail.com>
2017-09-27 23:52:42 +0800

    Config: Add more barriers for idiotproofness
    Add an empty macro check in each configuration file to ensure user does not select a configuration without changing the target device.
    
    Although I think I will be the idiot who forgets to do that...

--- inc/config/stm32f103_dev.h
+++ inc/config/stm32f103_dev.h
@@ -1,6 +1,10 @@
 #ifndef RTLIB_CONFIG_STM32F103_DEV_H_
 #define RTLIB_CONFIG_STM32F103_DEV_H_
 
+#if !EMPTY_MACRO(STM32F103VCT6)
+#error "This configuration is designed for a STM32F103VCT6 device. (Did you set DEVICE in CMakeLists.txt correctly?)"
+#endif  // !EMPTY_MACRO(STM32F103VCT6)
+
 #define LIB_USE_LED 1
 #define LIB_LED1_PINOUT {GPIOB, GPIO0}
 
