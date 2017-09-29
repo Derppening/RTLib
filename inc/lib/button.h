ef5388f8de73d4cc015017a240342c0170fb9f89
Derppening <david.18.19.21@gmail.com>
2017-09-29 15:01:54 +0800

    Button: Initial implementation
    Currently only works for the STM32F4xx devices. STM32F1xx device support
    will be added shortly.

---
+++ inc/lib/button.h
@@ -0,0 +1,123 @@
+#ifndef RTLIB_LIB_BUTTON_H_
+#define RTLIB_LIB_BUTTON_H_
+
+#include <cstdint>
+
+#include "config/config.h"
+#include "core/gpio.h"
+
+static_assert(LIB_USE_BUTTON, "Button library is disabButton in your configuration.");
+
+/**
+ * @brief HAL implementation for buttons.
+ * 
+ * This abstraction layer provides standard state-reading for onboard buttons. One button object is designed to manage
+ * one button on the mainboard.
+ */
+class Button {
+ public:
+  /**
+   * @brief Enumeration for different internal pull-up/pull-down states.
+   */
+  enum PullupState : uint8_t {
+    /**
+     * @brief Do not use the internal pull-up/pull-down resistor (i.e. floating mode).
+     * 
+     * Equivalent to libopencm3 macro @c GPIO_PUPD_NONE.
+     */
+    kNone = 0x0,
+    /**
+     * @brief Use the internal pull-up resistor.
+     * 
+     * Equivalent to libopencm3 macro @c GPIO_PUPD_PULLUP.
+     */
+    kPullUp = 0x1,
+    /**
+     * @brief Use the internal pull-down resistor.
+     * 
+     * Equivalent to libopencm3 macro @c GPIO_PUPD_PULLDOWN.
+     */
+    kPullDown = 0x2
+  };
+
+  /**
+   * @brief Configuration for button.
+   */
+  struct Config {
+    /**
+     * @brief ID of the button.
+     * 
+     * See your device configuration header file to see which id corresponds to which hardware button.
+     */
+    uint8_t id;
+    /**
+     * @brief Whether to use the internal pull-up/pull-down resistor.
+     * 
+     * You may use Button#PullupState enumeration or libopencm3 macros.
+     */
+    uint8_t pullup;
+  };
+
+  /**
+   * @brief Default constructor for Button.
+   *
+   * @param config Button configuration
+   */
+  explicit Button(const Config& config);
+  
+  /**
+   * @brief Default trivial destructor.
+   */
+  ~Button() = default;
+
+  /**
+   * @brief Move constructor for Button.
+   *
+   * @param other Button object to move from
+   */
+  Button(Button&& other) noexcept = default;
+  /**
+   * @brief Move assignment operator for Button.
+   *
+   * @param other Button object to move from
+   * @return Reference to the moved Button.
+   */
+  Button& operator=(Button&& other) noexcept = default;
+
+  /**
+   * @brief Copy constructor for Button.
+   *
+   * This constructor is deleted because there should only be one object managing each button, similar to @c
+   * std::unique_ptr.
+   */
+  Button(const Button&) = delete;
+  /**
+   * @brief Copy assignment operator for Button.
+   *
+   * This constructor is deleted because there should only be one object managing each button, similar to @c
+   * std::unique_ptr.
+   */
+  Button& operator=(const Button&) = delete;
+
+  /**
+   * @brief Reads the state of the button.
+   *
+   * @note Button will always be in low state at idle after constructor is calButton. If it isn't, your
+   * Button#Config#pullup may be incorrect; Set it to another value or consult a hardware senior if that is the case.
+   *
+   * @return @c true if button is pressed, @c false otherwise.
+   */
+  bool Read();
+
+ protected:
+  /**
+   * @return GPIO object which manages the pin of the button
+   */
+  GPIO* GetGpio() { return &gpio_; }
+
+ private:
+  GPIO gpio_;
+  bool polarity_;
+};
+
+#endif  // RTLIB_LIB_BUTTON_H_
---
+++ src/lib/button.cpp
@@ -0,0 +1,49 @@
+#include "config/config.h"
+
+#if defined(LIB_USE_BUTTON) && (EMPTY_MACRO(LIB_USE_BUTTON) != 1)
+
+#include "lib/button.h"
+
+#include <cassert>
+
+namespace {
+inline Pinout GetConfigPinout(const uint8_t id) {
+  assert(id < LIB_USE_BUTTON);
+  switch (id) {
+    default:
+      // not handled, since assert will catch this error
+      assert(false);
+      break;
+#if LIB_USE_BUTTON > 0
+    case 0:
+      return LIB_BUTTON0_PINOUT;
+#endif  // LIB_USE_BUTTON > 0
+#if LIB_USE_BUTTON > 1
+    case 1:
+      return LIB_BUTTON1_PINOUT;
+#endif  // LIB_USE_BUTTON > 1
+#if LIB_USE_BUTTON > 2
+    case 2:
+      return LIB_BUTTON2_PINOUT;
+#endif  // LIB_USE_BUTTON > 2
+  }
+}
+}  // namespace
+
+Button::Button(const Config& config) :
+#if defined(STM32F1)
+#error "STM32F1xx configuration incomplete. Please file a bug report on Github."
+// TODO(Derppening): STM32F1 button config
+#elif defined(STM32F4)
+    gpio_(GetConfigPinout(config.id), GPIO_MODE_INPUT, config.pullup, GPIO_OSPEED_50MHZ),
+#endif
+    polarity_(config.pullup == PullupState::kPullUp) {
+}
+
+bool Button::Read() {
+  return static_cast<bool>(gpio_.Read() ^ polarity_);
+}
+
+#else
+#error "LIB_USE_BUTTON macro not found. (Did you define it in your board configuration?)"
+#endif  // defined(LIB_USE_BUTTON) && (EMPTY_MACRO(LIB_USE_BUTTON) != 1)
