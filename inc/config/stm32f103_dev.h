4b25b7a6204db18636773f87e94d83c328471282
David Mak <david.18.19.21@gmail.com>
2017-10-16 20:19:13 +0800

    Config: Fix STM32F103 Development Board configuration
    Redefine LIB_USE_BUTTON.

--- inc/config/stm32f103_dev.h
+++ inc/config/stm32f103_dev.h
@@ -1,11 +1,13 @@
 #ifndef RTLIB_CONFIG_STM32F103_DEV_H_
 #define RTLIB_CONFIG_STM32F103_DEV_H_
 
-#if !EMPTY_MACRO(STM32F103VCT6)
+#if !defined(STM32F103VCT6)
 #error "This configuration is designed for a STM32F103VCT6 device. (Did you set DEVICE in CMakeLists.txt correctly?)"
-#endif  // !EMPTY_MACRO(STM32F103VCT6)
+#endif  // !defined(STM32F103VCT6)
 
 #define LIB_USE_LED 1
 #define LIB_LED0_PINOUT {GPIOB, GPIO0}
+
+#define LIB_USE_BUTTON 0
 
 #endif  // RTLIB_CONFIG_STM32F103_DEV_H_
