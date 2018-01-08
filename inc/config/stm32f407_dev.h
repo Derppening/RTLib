c7d418fd058e85fb8f1c5e5ef82f32922b5583b3
David Mak <david.18.19.21@gmail.com>
2018-01-08 21:21:58 +0800

    Config: Fix STM32F407_DEV configuration
    In 46b66f5, EMPTY_MACRO is removed, but the changes were not applied to
    the STM32F407 dev board configuration. Fixing this in this commit.

--- inc/config/stm32f407_dev.h
+++ inc/config/stm32f407_dev.h
@@ -22,9 +22,9 @@
 #ifndef RTLIB_CONFIG_STM32F407_DEV_H_
 #define RTLIB_CONFIG_STM32F407_DEV_H_
 
-#if !EMPTY_MACRO(STM32F407VET6)
+#if !defined(STM32F407VET6)
 #error "This configuration is designed for a STM32F407VET6 device. (Did you set DEVICE in CMakeLists.txt correctly?)"
-#endif  // !EMPTY_MACRO(STM32F407VET6)
+#endif  // !defined(STM32F407VET6)
 
 #define LIB_USE_LED 2
 #define LIB_LED0_PINOUT {GPIOA, GPIO6}
