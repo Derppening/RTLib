63337d9f8b0678a2cc38fe9fb662c9394c61f1a6
David Mak <david.18.19.21@gmail.com>
2018-01-08 21:16:29 +0800

    CMake: Update warning flags again

--- cmake/compile_flags.cmake
+++ cmake/compile_flags.cmake
@@ -1,18 +1,26 @@
+# Enforce C/C++ standard level
 set(CMAKE_C_STANDARD_REQUIRED YES)
 set(CMAKE_CXX_STANDARD_REQUIRED YES)
 
-set(COMMON_WARN_FLAGS "-pedantic -Wall -Wextra \
--Wcast-align -Wcast-qual -Wdisabled-optimization -Wdouble-promotion -Wduplicated-cond -Wfloat-equal -Wformat=2 \
--Winit-self -Winline -Winvalid-pch -Wlogical-op -Wmissing-declarations -Wmissing-include-dirs \
--Wmissing-format-attribute -Wno-unused -Wnull-dereference -Wodr -Wpointer-arith -Wredundant-decls -Wshadow \
--Wsign-conversion -Wstrict-overflow=5 -Wswitch-default -Wswitch-enum -Wwrite-strings -Wundef -Wuninitialized \
--Wunreachable-code")
+# Enforce to disable any compiler-specific extensions
+set(CMAKE_C_EXTENSIONS NO)
+set(CMAKE_CXX_EXTENSIONS NO)
 
-set(C_WARN_FLAGS "${COMMON_WARN_FLAGS} -Wbad-function-cast -Wnested-externs -Wmissing-prototypes -Wold-style-definition \
--Wstrict-prototypes")
-set(CXX_WARN_FLAGS "${COMMON_WARN_FLAGS} -Wctor-dtor-privacy -Wnoexcept -Wold-style-cast -Woverloaded-virtual \
--Wsign-promo -Wstrict-null-sentinel -Wuseless-cast -Wzero-as-null-pointer-constant")
+# Flags that apply to both C and C++
+set(COMMON_WARN_FLAGS "${COMMON_WARN_FLAGS} -pedantic -Wall -Wextra\
+    -Wcast-align -Wcast-qual -Wconversion -Wdisabled-optimization -Wdouble-promotion -Wduplicated-cond\
+    -Wduplicated-branches -Wfloat-equal -Wformat=2 -Winit-self -Winline -Winvalid-pch -Wlogical-op\
+    -Wmissing-declarations -Wmissing-format-attribute -Wmissing-include-dirs -Wno-unused -Wnull-dereference -Wodr\
+    -Wpointer-arith -Wredundant-decls -Wrestrict -Wshadow -Wsign-conversion -Wstrict-overflow=5 -Wswitch-default\
+    -Wswitch-enum -Wwrite-strings -Wundef -Wuninitialized -Wunreachable-code")
 
+# Flags that apply only to C OR C++
+set(C_WARN_FLAGS "${COMMON_WARN_FLAGS} -Wbad-function-cast -Wmissing-prototypes -Wnested-externs\
+    -Wold-style-definition -Wstrict-prototypes")
+set(CXX_WARN_FLAGS "${COMMON_WARN_FLAGS} -Wctor-dtor-privacy -Wnoexcept -Wold-style-cast -Woverloaded-virtual\
+    -Wsign-promo -Wstrict-null-sentinel -Wuseless-cast -Wzero-as-null-pointer-constant")
+
+# Pass them back to the CMake variable
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${C_WARN_FLAGS}")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CXX_WARN_FLAGS}")
 
