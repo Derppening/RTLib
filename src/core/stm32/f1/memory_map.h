230b490639ed99278a4c57ddca90ee6d1c27ff90
David Mak <david.18.19.21@gmail.com>
2019-01-09 22:57:38 +0800

    [memory_map] Extract get/set memory functions

--- src/core/stm32/f1/memory_map.h
+++ src/core/stm32/f1/memory_map.h
@@ -28,31 +28,9 @@
 #include <cstdint>
 #include <type_traits>
 
+#include "core/memory_map.h"
+
 namespace rtlib::core::stm32::f1 {
-
-/**
- * @brief Loads data from the specified address.
- *
- * @tparam T Length of data to retrieve. Must be an integral type.
- * @param base_addr Address to load from.
- * @return Data from the specified address, represented as T.
- */
-template<typename T = std::uint32_t, typename = std::enable_if_t<std::is_integral_v<T>>>
-inline constexpr volatile T const get_mem(const std::uint32_t base_addr) {
-  return *reinterpret_cast<volatile T* const>(base_addr);
-}
-
-/**
- * @brief Stores data into the registered address.
- *
- * @tparam T Length of data to retrieve. Must be an integral type.
- * @param base_addr Address to store into.
- * @param val Data to store.
- */
-template<typename T = std::uint32_t, typename = std::enable_if_t<std::is_integral_v<T>>>
-inline constexpr void set_mem(const std::uint32_t base_addr, const T val) {
-  *reinterpret_cast<volatile T* const>(base_addr) = val;
-}
 
 enum struct reg {
   // Peripheral Registers
